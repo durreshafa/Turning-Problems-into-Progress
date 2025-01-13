b4540e29dd9849a4e31dca05c855ed07df471922
Durre Shafa <dureshafa@gmail.com>
2025-01-13 15:46:45 +0500

    Time: 3 ms (20.67%) | Memory: 12.5 MB (32.12%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def runningSum(self, nums):
+        total = 0  
+        result = []  
+        for num in nums:
+            total += num  
+            result.append(total)  
+        return result
+        
