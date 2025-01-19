ae2b67106dec1c777a036911b1e0958fb7ca097c
Durre Shafa <dureshafa@gmail.com>
2025-01-19 16:05:25 +0500

    Time: 0 ms (100.00%) | Memory: 23.1 MB (11.79%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+       s.reverse()
+       return s
+    #in place algorithm: hmny apni existing error ko modify krna hy
+        
