fa68a1490fdddd86b3500589c8027bdf6514f96e
Durre Shafa <dureshafa@gmail.com>
2025-01-23 18:13:27 +0500

    Time: 4 ms (14.91%) | Memory: 18 MB (15.72%) - LeetSync

---
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumOfSquares(self, nums: List[int]) -> int:
+        n = len(nums)
+        total_sum = 0
+        for i in range(1, n + 1):
+            if n % i == 0:
+                total_sum += nums[i - 1] * nums[i - 1]  
+        return total_sum
+        
