641c22147d85d25ffb9ea82e5ad096fb1b93edee
Durre Shafa <dureshafa@gmail.com>
2025-02-06 19:02:34 +0500

    Time: 3 ms (43.42%) | Memory: 23 MB (73.42%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -1,6 +1,14 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-       s.reverse()
-       return s
-    #in place algorithm: hmny apni existing error ko modify krna hy
+    def reverseString(self, nums: List[str]) -> None:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            nums[l] , nums[r] = nums[r] , nums[l]
+            l += 1
+            r -= 1
+
+    #    s.reverse()
+    #    return s
+    # #in place algorithm: hmny apni existing error ko modify krna hy
+        
         
