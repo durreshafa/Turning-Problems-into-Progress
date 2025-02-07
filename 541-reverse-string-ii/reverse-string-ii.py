48deac201ede265af7c7c160c4d5abe65403cccb
Durre Shafa <dureshafa@gmail.com>
2025-02-07 19:25:29 +0500

    Time: 3 ms (36.83%) | Memory: 17.8 MB (63.68%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.py
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -1,8 +1,20 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        s = list(s)
+        s = list(s)  
         n = len(s)
-        for i in range(0, n, 2 * k):
-            s[i:i + k] = reversed(s[i:i + k])
-        return ''.join(s)
+        for i in range(0, n, 2 * k):  
+            left, right = i, min(i + k - 1, n - 1)  
+
+            while left < right:  
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+
+        return "".join(s)  
+        
+        # s = list(s)
+        # n = len(s)
+        # for i in range(0, n, 2 * k):
+        #     s[i:i + k] = reversed(s[i:i + k])
+        # return ''.join(s)
 
